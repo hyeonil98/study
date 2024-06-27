786b38d9697e965335f8663d96876bfde75edd1e
TwoEther <lhi0711@naver.com>
2024-06-27 15:22:40 +0900

    normalization

---
+++ spring/src/main/java/com/example/proj/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.example.proj.controller;
+
+import com.example.domain.PostRender;
+import com.example.proj.repository.BoardJpaRepository;
+import com.example.proj.repository.CafeMemberJpaRepository;
+import com.example.proj.repository.PostJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/v1/cafe/{cafeId}/boards/{boardId}/posts")
+public class PostController {
+    private final CafeMemberJpaRepository cafeMemberJpaRepository;
+    private final BoardJpaRepository boardJpaRepository;
+    private final PostJpaRepository postJpaRepository;
+    private final PostRender postRender;
+
+}
---
+++ spring/src/main/java/com/example/proj/repository/CafeMemberJpaRepository.java
@@ -0,0 +1,7 @@
+package com.example.proj.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CafeMemberJpaRepository {
+}
